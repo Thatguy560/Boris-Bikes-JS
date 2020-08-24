41326f20a913edc03952eb7056bae02670874086
Peter Stevens <Peterstevens510@gmail.com>
2020-08-24 18:34:03 +0100

    Adding comments on how code works

--- src/dockingStation.js
+++ src/dockingStation.js
@@ -22,12 +22,13 @@ class DockingStation {
     this.bikesStored.some((bike, index) => {
       if (bike.bikeWorking()) {
         this.bikesStored.splice(index, 1); // remove 1 items at position index.
-        isBikeReleased = true;
-        bikeToBeReturned = bike;
+        isBikeReleased = true; // setting 'isBikeReleased' from false to true
+        bikeToBeReturned = bike; // once we set 'isBikeReleased' to be true we then set bikeToBeReturned from nothing to a new instance of Bike.
         return bike;
       }
     });
     if (!isBikeReleased) {
+      // !isBikeReleased = true, meaning bike will try to be released but it won't as bikeToBeReturned is now set back to null again.
       throw new Error("No working bikes currently available");
     }
     return bikeToBeReturned; // shows you the bike you're releasing
